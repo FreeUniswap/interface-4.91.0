47e1752aabfeea65e051d9e5c1e71bc15093de79
Ricardus <richitrader1@hotmail.com>
2023-11-16 02:30:24 +0100

    Initial commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,156 @@
+import { initializeAnalytics } from 'components/AmplitudeAnalytics'
+import { PageName } from 'components/AmplitudeAnalytics/constants'
+import { Trace } from 'components/AmplitudeAnalytics/Trace'
+import Loader from 'components/Loader'
+import TopLevelModals from 'components/TopLevelModals'
+import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
+import { lazy, Suspense } from 'react'
+import { useEffect } from 'react'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import styled from 'styled-components/macro'
+
+import { useAnalyticsReporter } from '../components/analytics'
+import ErrorBoundary from '../components/ErrorBoundary'
+import Header from '../components/Header'
+import Polling from '../components/Header/Polling'
+import Popups from '../components/Popups'
+import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
+import AddLiquidity from './AddLiquidity'
+import { RedirectDuplicateTokenIds } from './AddLiquidity/redirects'
+import { RedirectDuplicateTokenIdsV2 } from './AddLiquidityV2/redirects'
+import Earn from './Earn'
+import Manage from './Earn/Manage'
+import MigrateV2 from './MigrateV2'
+import MigrateV2Pair from './MigrateV2/MigrateV2Pair'
+import Pool from './Pool'
+import { PositionPage } from './Pool/PositionPage'
+import PoolV2 from './Pool/v2'
+import PoolFinder from './PoolFinder'
+import RemoveLiquidity from './RemoveLiquidity'
+import RemoveLiquidityV3 from './RemoveLiquidity/V3'
+import Swap from './Swap'
+import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
+
+// lazy load vote related pages
+const Vote = lazy(() => import('./Vote'))
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+`
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 120px 16px 0px 16px;
+  align-items: center;
+  flex: 1;
+  z-index: 1;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    padding: 4rem 8px 16px 8px;
+  `};
+`
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+`
+
+const Marginer = styled.div`
+  margin-top: 5rem;
+`
+
+function getCurrentPageFromLocation(locationPathname: string): PageName | undefined {
+  switch (locationPathname) {
+    case '/swap':
+      return PageName.SWAP_PAGE
+    case '/vote':
+      return PageName.VOTE_PAGE
+    case '/pool':
+      return PageName.POOL_PAGE
+    default:
+      return undefined
+  }
+}
+
+export default function App() {
+  const { pathname } = useLocation()
+  const currentPage = getCurrentPageFromLocation(pathname)
+  useAnalyticsReporter()
+  initializeAnalytics()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return (
+    <ErrorBoundary>
+      <DarkModeQueryParamReader />
+      <ApeModeQueryParamReader />
+      <AppWrapper>
+        <Trace page={currentPage}>
+          <HeaderWrapper>
+            <Header />
+          </HeaderWrapper>
+          <BodyWrapper>
+            <Popups />
+            <Polling />
+            <TopLevelModals />
+            <Suspense fallback={<Loader />}>
+              <Routes>
+                <Route path="vote/*" element={<Vote />} />
+                <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} />
+                <Route path="claim" element={<OpenClaimAddressModalAndRedirectToSwap />} />
+                <Route path="uni" element={<Earn />} />
+                <Route path="uni/:currencyIdA/:currencyIdB" element={<Manage />} />
+
+                <Route path="send" element={<RedirectPathToSwapOnly />} />
+                <Route path="swap/:outputCurrency" element={<RedirectToSwap />} />
+                <Route path="swap" element={<Swap />} />
+
+                <Route path="pool/v2/find" element={<PoolFinder />} />
+                <Route path="pool/v2" element={<PoolV2 />} />
+                <Route path="pool" element={<Pool />} />
+                <Route path="pool/:tokenId" element={<PositionPage />} />
+
+                <Route path="add/v2" element={<RedirectDuplicateTokenIdsV2 />}>
+                  <Route path=":currencyIdA" />
+                  <Route path=":currencyIdA/:currencyIdB" />
+                </Route>
+                <Route path="add" element={<RedirectDuplicateTokenIds />}>
+                  {/* this is workaround since react-router-dom v6 doesn't support optional parameters any more */}
+                  <Route path=":currencyIdA" />
+                  <Route path=":currencyIdA/:currencyIdB" />
+                  <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
+                </Route>
+
+                <Route path="increase" element={<AddLiquidity />}>
+                  <Route path=":currencyIdA" />
+                  <Route path=":currencyIdA/:currencyIdB" />
+                  <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
+                  <Route path=":currencyIdA/:currencyIdB/:feeAmount/:tokenId" />
+                </Route>
+
+                <Route path="remove/v2/:currencyIdA/:currencyIdB" element={<RemoveLiquidity />} />
+                <Route path="remove/:tokenId" element={<RemoveLiquidityV3 />} />
+
+                <Route path="migrate/v2" element={<MigrateV2 />} />
+                <Route path="migrate/v2/:address" element={<MigrateV2Pair />} />
+
+                <Route path="*" element={<RedirectPathToSwapOnly />} />
+              </Routes>
+            </Suspense>
+            <Marginer />
+          </BodyWrapper>
+        </Trace>
+      </AppWrapper>
+    </ErrorBoundary>
+  )
+}
